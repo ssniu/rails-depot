ed84d5c88fa0b2d65ee0956599d991e177f99875
ssniu <ssniu2011@gmail.com>
2016-06-27 19:18:44 -0400

    Rails depot

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,10 @@
 class Product < ActiveRecord::Base
+  validates :title, :description, :image_url, presence: true
+  # validates() is the standard Rails validator. It will check one or moure
+  # model fields against one or more conditions
+  # presence:true tells the validator to check that each of the named fields is present
+  # and its contents are not empty.
+
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  # numericality() option to verify that the price is a valid number
 end
