3fb18cb87a6444c3fb5cca9f33ca5c3809f9bf27
ssniu <ssniu2011@gmail.com>
2016-07-06 22:27:57 -0400

    add translation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  get 'admin' => 'admin#index'
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
+
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   resources :users
   resources :orders
   resources :line_items
@@ -9,13 +22,21 @@ Rails.application.routes.draw do
 
   resources :products do
     get :who_bought, on: :member
-  end 
+  end
+
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store', via: :all
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'store#index', as: 'store'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
